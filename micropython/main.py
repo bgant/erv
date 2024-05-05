28bc31b406385c8e75d4ed432cb4d687983eebd5
Brandon Gant <bgant@protonmail.com>
2024-05-04 21:29:45 -0500

    refactor and cleanup

--- micropython/main.py
+++ micropython/main.py
@@ -6,7 +6,6 @@ mpremote a0 mip install --target= github:bgant/micropython/modules/key_store.py
 mpremote a0 mip install --target= github:bgant/micropython/modules/timezone.py
 mpremote a0 mip install --target= github:bgant/micropython/modules/AirNowAPI.py
 mpremote a0 mip install --target= github:bgant/micropython/modules/OpenWeatherMap.py
-mpremote a0 mip install --target= github:bgant/micropython/modules/PMS7003.py
 mpremote a0 mip install --target= github:bgant/micropython/modules/webdis.py
 '''
 
@@ -21,7 +20,7 @@ wifi.connect()
 
 # Import Project Specific Modules
 from vttouchw import VTTOUCHW
-from utime import localtime, sleep
+from utime import time, localtime, sleep
 from timezone import tz
 
 # If hitting remote API's directly
@@ -40,18 +39,19 @@ class PROJECT:
         #self.weather = WEATHER()
         #self.aqi = AQI()
         #self.PM_EPA = self.aqi.download('PM')  # Download on boot
-        #self.pms7003 = PMS7003()
         self.temp = WEBDIS()
-        self.aqi = WEBDIS()
+        self.epa_aqi = WEBDIS()
+        self.local_aqi = WEBDIS()
 
         # Thresholds
-        self.spring      = 106 # Beginning of Summer Hours (Apr 15)
-        self.fall        = 289 # Beginning of Winter Hours (Oct 15)
-        self.night_end   =  6  # ERV  on after 6AM
-        self.night_start = 22  # ERV off after 10PM
-        self.too_cold    = 32  # ERV off below 32F
-        self.too_hot     = 90  # ERV off above 90F
-        self.high_aqi    = 100 # ERV off above 100 PM2.5
+        self.spring         = 106  # Beginning of Summer Hours (Apr 15)
+        self.fall           = 289  # Beginning of Winter Hours (Oct 15)
+        self.night_end      =   6  # ERV  on after 6AM
+        self.night_start    =  22  # ERV off after 10PM
+        self.too_cold       =  32  # ERV off below 32F
+        self.too_hot        =  90  # ERV off above 90F
+        self.high_epa_aqi   = 100  # ERV off above 100 PM2.5
+        self.high_local_aqi =  40  # ERV off above  40 PM2.5
 
     def night(self):
         '''Is it night right now?'''
@@ -68,12 +68,20 @@ class PROJECT:
 
     def outside_too_hot_or_cold(self):
         '''Is it too hot or cold outside right now?'''
+        # Using OpenWeatherMap API:
         #self.outside_temp = self.weather.download('temp')
-        self.temp.get('nws-temperature')  # Get temperature from local Redis/Webdis server
-        self.outside_temp = int(self.temp.response_text)
+
+        # Using local Webdis/Redis:
+        try:
+            self.temp.get('nws-temperature')  # Get temperature from local Redis/Webdis server
+            self.outside_temp = int(self.temp.response_text)
+        except:
+            print('ERROR: Failed to access National Weather Service data')
+            return False 
+
         if self.outside_temp:
             if (self.outside_temp < self.too_cold) or (self.outside_temp > self.too_hot):
-                print(f'OFF: Too hot or cold at {self.outside_temp:.0f}F')
+                print(f'OFF: Too hot or cold at {self.outside_temp:.0f} F')
                 return True
             else:
                 print(f'OK:  Outside temperature is good at {self.outside_temp:.0f}F')
@@ -84,15 +92,22 @@ class PROJECT:
 
     def epa_aqi_bad(self):
         '''Is the EPA Air Quality Index too high right now?'''
+        # Using AirNowAPI (data updates about 10 to 30 minutes after each hour):
         #if 20 < localtime(tz())[4] < 30:
-            # Data updates about 10 to 30 minutes after each hour
-            #self.PM_EPA = self.aqi.download('PM')
-        self.aqi.get('json-epa-aqi')  # Get AQI from local Redis/Webdis server
-        self.PM_EPA = self.aqi.response_json[0]['AQI']
+        #    self.PM_EPA = self.aqi.download('PM')
+
+        # Using local Webdis/Redis:
+        try:
+            self.epa_aqi.get('json-epa-aqi')
+            self.PM_EPA = self.epa_aqi.response_json[0]['AQI']
+        except:
+            print('ERROR: Failed to access EPA AQI data')
+            return False
+
         if not self.PM_EPA:
             print(f'ON:  EPA Air Quality Unknown (no data from API)') 
             return False
-        elif self.PM_EPA > self.high_aqi:
+        elif self.PM_EPA > self.high_epa_aqi:
             print(f'OFF: EPA Air Quality is too high at {self.PM_EPA}')
             return True
         else:
@@ -101,15 +116,25 @@ class PROJECT:
 
     def local_aqi_bad(self):
         '''Is the outside Air Quality device to high right now? (neighbors burning leaves?)'''
-        # self.PM_Local = self.pms7003.check()
-        # if self.PM_Local > self.high_aqi:
-        #     print(f'OFF: Local Air Quality is too high at {self.PM_Local}')
-        #     return True    
-        # else:
-        #     print(f'OK:  Local Air Quality is good at {self.PM_Local}')
-        #     return False
-        print('OK:  Local AQI device not installed yet')
-        return False
+        # Using local Webdis/Redis:
+        try:
+            self.local_aqi.timeseriesget('webdis-local-aqi-average')
+            aqi_timestamp = int(self.local_aqi.response_text[0]/1000) - 946684800  #13-digit Unix to 9-digit Micropython
+            self.local_aqi = float(self.local_aqi.response_text[1])
+        except:
+            print('ERROR: Failed to access Local AQI data')
+            return False
+
+        if (time() - aqi_timestamp) < 180:  # AQI is recent?
+            if self.local_aqi >= self.high_local_aqi:
+                print(f'OFF: Local Air Quality is too high at {self.local_aqi:.0f}')
+                return True
+            else:
+                print(f'OK:  Local Air Quality is good at {self.local_aqi:.0f}')
+                return False
+        else:
+            print('OK:  Ignoring... No recent Local AQI data')
+            return False
     
     def control(self):
         '''Check everything and decide whether to turn ERV On or Off'''
@@ -121,6 +146,7 @@ class PROJECT:
         else:
             print('OFF: One or More Checks Failed')
             self.standby()
+        print()
 
     def smart(self):
         '''Change ERV mode to Smart'''
